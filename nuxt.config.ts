17a94f9e654886875bc566562797afd3d07625ff
Danil Ukhov <danil.u@bnovo.ru>
2025-09-20 08:44:47 +0800

    feat: add Sass preprocessor for styling
    - Install sass package for SCSS support
    - Create assets/styles/main.scss with global styles
    - Add test CSS variables, mixins, and utility classes
    - Configure Nuxt to load global SCSS file
    
    Time spent: 8 min

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/fonts']
+  modules: ['@nuxt/image', '@nuxt/fonts'],
+  css: ['~/assets/styles/main.scss']
 })
